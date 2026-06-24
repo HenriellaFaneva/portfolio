367c9afd80a9505ababf0794230f0a4a3e00170d
todisoapatrick <fanevahenriella@gmail.com>
2026-06-24 10:15:36 +0300

    portfolio fix head google verification

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Henriella Faneva Randriamihajarivo | Portfolio</title>
+      <title>Randriamihajarivo Henriella Faneva| Portfolio</title>
       <meta
         name="google-site-verification"
         content="HuAsnY_JrInXSxi4-CYlSgIeRkYUJ82ynTOT-G7n5PM"
